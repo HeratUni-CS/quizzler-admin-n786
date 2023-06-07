ae0dff1ea23a038f45795a9d712aafaabb73dcc4
admin-n786 <movahhedd786@gmail.com>
2023-06-07 04:31:07 -0700

    added new code

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,32 @@ class Quizzler extends StatelessWidget {SSS
     );
   }
 }
+
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
